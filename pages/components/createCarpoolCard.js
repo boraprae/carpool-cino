86d37d312be12fbebfd3de4714e9bf928f7c1822
boraprae <72434691+boraprae@users.noreply.github.com>
2022-11-29 23:45:11 +0700

    card for carpool creation

--- pages/components/createCarpoolCard.js
+++ pages/components/createCarpoolCard.js
@@ -30,7 +30,7 @@ export default function CreateCarpoolCard() {
     <>
       <Card
         sx={{
-          maxWidth: 350,
+          width: "1200px",
           mt: 4,
           boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.1)",
           borderRadius: "15px",
@@ -40,68 +40,71 @@ export default function CreateCarpoolCard() {
         <CardContent>
           <Stack
             direction="row"
-            justifyContent="flex-start"
-            alignItems="center"
-            spacing={1}
-            mb={1}
-          >
-            <Avatar
-              alt="Cindy Baker"
-              src="/assets/profileImg.jpg"
-              sx={{ width: 24, height: 24 }}
-            />
-            <Typography sx={{ fontSize: "14px" }} color="text.secondary">
-              Jane Kim
-            </Typography>
-          </Stack>
-          <Divider />
-          <Stack
-            direction="row"
             justifyContent="space-between"
-            alignItems="center"
+            alignItems="flex-start"
           >
-            <Typography sx={{ fontSize: "14px", mt: 1 }}>
-              Destination
-            </Typography>
-            <Typography sx={{ fontSize: "14px", mt: 1 }}>Seat</Typography>
-          </Stack>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Typography sx={{ fontSize: "18px", fontWeight: "bold" }}>
-              Central Plaza Chiang Rai
-            </Typography>
-            <Typography sx={{ fontSize: "18px" }}>1/4</Typography>
+            <Stack
+              direction="column"
+              justifyContent="flex-start"
+              alignItems="flex-start"
+            >
+              <Typography sx={{ fontSize: "16px", mt: 1, fontWeight: "bold" }}>
+                Car Information
+              </Typography>
+              <Typography sx={{ fontSize: "14px" }}>
+                Mitsubishi กขค 123 เชียงราย
+              </Typography>
+              <Typography sx={{ fontSize: "16px", mt: 1, fontWeight: "bold" }}>
+                Departure Date
+              </Typography>
+              <Typography sx={{ fontSize: "14px" }}>2 Nov 2022</Typography>
+            </Stack>
+
+            <Stack
+              direction="column"
+              justifyContent="flex-start"
+              alignItems="flex-start"
+            >
+              <Typography sx={{ fontSize: "16px", mt: 1, fontWeight: "bold" }}>
+                Origin
+              </Typography>
+              <Typography sx={{ fontSize: "14px" }}>
+                Mae Fah Luang University
+              </Typography>
+
+              <Typography sx={{ fontSize: "16px", mt: 1, fontWeight: "bold" }}>
+                Departure Time
+              </Typography>
+              <Typography sx={{ fontSize: "14px" }}>5 P.M.</Typography>
+            </Stack>
+            <Stack
+              direction="column"
+              justifyContent="flex-start"
+              alignItems="flex-start"
+            >
+              <Typography sx={{ fontSize: "16px", mt: 1, fontWeight: "bold" }}>
+                Destination
+              </Typography>
+              <Typography sx={{ fontSize: "14px" }}>
+                Central Plaza Chiang Rai
+              </Typography>
+            </Stack>
           </Stack>
 
+          <Stack
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+          >
+          </Stack>
+          <Typography sx={{ fontSize: "16px", mt: 1, fontWeight: "bold" }}>
+            Description
+          </Typography>
           <Typography sx={{ fontSize: "14px", mt: 1 }}>
             From Mae Fah Luang University, can pick up you anywhere around the
             university
           </Typography>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Typography sx={{ fontSize: "14px", mt: 1, fontWeight: "bold" }}>
-              Departure Date
-            </Typography>
-            <Typography sx={{ fontSize: "14px", mt: 1, fontWeight: "bold" }}>
-              Departure Time
-            </Typography>
-          </Stack>
-
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Typography sx={{ fontSize: "16px" }}>2 Nov 2022</Typography>
-            <Typography sx={{ fontSize: "16px" }}>5 P.M.</Typography>
-          </Stack>
-          <Divider sx={{mb:2, mt:2}} />
+          <Divider sx={{ mb: 2, mt: 2 }} />
           <BookingButton>Booking</BookingButton>
         </CardContent>
       </Card>
