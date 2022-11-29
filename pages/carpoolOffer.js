9c866b3ec35e7b6d8946c91d274cdb851581c78e
boraprae <72434691+boraprae@users.noreply.github.com>
2022-11-29 23:22:52 +0700

    implement css layout

--- pages/carpoolOffer.js
+++ pages/carpoolOffer.js
@@ -1,10 +1,29 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Typography, Button } from "@mui/material";
 import Sidebar from "./components/sidebar";
+import styles from "../styles/Home.module.css";
+import CreateCarpoolCard from "./components/createCarpoolCard";
 
 export default function carPoolOffer() {
   return (
-    <>
+    <main className={styles.main}>
       <Sidebar />
-    </>
+      <Typography
+        sx={{
+          fontWeight: 700,
+          fontSize: "24px",
+          lineHeight: "31px",
+          color: "#504E4E",
+        }}
+      >
+        My Carpool Offers
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "#58CAD2", px: 2, py: 1, my: 2 }}
+      >
+        Create Offer
+      </Button>
+      <CreateCarpoolCard/>
+    </main>
   );
 }
